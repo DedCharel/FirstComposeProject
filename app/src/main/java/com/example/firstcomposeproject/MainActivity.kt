1a907eeb637c2df286d90f625b148a5cf878671d
Daniil Shcherbakov <Charel@bk.ru>
2025-03-14 11:05:46 +0300

    Test LazyColumn

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,33 +3,57 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.example.firstcomposeproject.ui.theme.InstagramProfileCard
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
-            FirstComposeProjectTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
-                ) {
-                    InstagramProfileCard(viewModel)
-                }
-
-            }
+            Test(viewModel = viewModel)
         }
     }
 
+}
+
+@Composable
+private fun Test(viewModel: MainViewModel){
+    FirstComposeProjectTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            LazyColumn {
+                item {
+                    Text(text = "Title")
+                }
+                items(10){
+                    InstagramProfileCard(viewModel)
+                }
+                item {
+                    Image(painter = painterResource(id = R.drawable.ic_instagram), contentDescription = "")
+                }
+                items(20){
+                    InstagramProfileCard(viewModel) 
+                }
+            }
+
+        }
+
+    }
 }
 
 
