2dd782704fef07eaaa0909b04e6933c46f1e6708
Daniil Shcherbakov <Charel@bk.ru>
2025-03-15 16:37:04 +0300

    4.9 Swipe to dismiss

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,21 +3,30 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxState
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.example.firstcomposeproject.ui.theme.InstagramProfileCard
-import androidx.compose.material3.Text
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +39,7 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Test(viewModel: MainViewModel){
     FirstComposeProjectTheme {
@@ -40,13 +50,54 @@ private fun Test(viewModel: MainViewModel){
         ) {
             val models = viewModel.models.observeAsState(listOf())
             LazyColumn {
-                items(models.value){ model ->
-                    InstagramProfileCard(
-                        model = model,
-                        onFollowedButtonClickListener = {
-                            viewModel.changeFollowingStatus(it)
+                items(models.value, key = {it.id}){ model ->
+                    val dismissThresholds = with(receiver = LocalDensity.current) {
+                        LocalConfiguration.current.screenWidthDp.dp.toPx() * 0.5F
+                    }
+
+                    val dismissState = SwipeToDismissBoxState(
+                        positionalThreshold = {dismissThresholds},
+                        initialValue = SwipeToDismissBoxValue.Settled,
+                        density = LocalDensity.current,
+                        confirmValueChange = {value ->
+                            val isDismissed = value in setOf(
+                                SwipeToDismissBoxValue.EndToStart,
+                                )
+                            if (isDismissed){
+                                viewModel.delete(model)
+                                true
+                            }
+                            false
+
                         }
                     )
+                    SwipeToDismissBox(
+                        state = dismissState,
+                        backgroundContent = {
+                            Box(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxSize()
+                                    .background(Color.Red.copy(alpha = 0.7F)),
+                                contentAlignment = Alignment.CenterEnd){
+                                
+                                Text(
+                                    modifier = Modifier.padding(16.dp),
+                                    text = "Delete Item",
+                                    color = Color.White,
+                                    fontSize = 14.sp)
+                            }
+                        }
+                    )
+                    {
+                        InstagramProfileCard(
+                            model = model,
+                            onFollowedButtonClickListener = {
+                                viewModel.changeFollowingStatus(it)
+                            }
+                        )
+                    }
+
                 }
 
             }
--- app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileFile.kt
+++ app/src/main/java/com/example/firstcomposeproject/ui/theme/InstagramProfileFile.kt
@@ -19,10 +19,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,7 +29,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.firstcomposeproject.InstagramModel
-import com.example.firstcomposeproject.MainViewModel
 import com.example.firstcomposeproject.R
 
 
